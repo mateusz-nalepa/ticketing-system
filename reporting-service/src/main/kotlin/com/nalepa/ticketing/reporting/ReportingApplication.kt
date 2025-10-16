0de5cc63c99b65539bf4913ca6b13015d042dad8
Mateusz Nalepa <mateuszcholyn@gmail.com>
2025-10-16 19:39:45 +0200

    init commit with apps

---
+++ reporting-service/src/main/kotlin/com/nalepa/ticketing/reporting/ReportingApplication.kt
@@ -0,0 +1,11 @@
+package com.nalepa.ticketing.reporting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReportingApplication>(*args)
+}
