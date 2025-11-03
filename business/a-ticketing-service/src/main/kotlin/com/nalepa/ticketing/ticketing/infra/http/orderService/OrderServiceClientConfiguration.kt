80af8854926cd509403bac8e1423206baf9316f4
Mateusz Nalepa <mateuszcholyn@gmail.com>
2025-11-03 20:47:29 +0100

    in tempo, full graph is visible

--- business/a-ticketing-service/src/main/kotlin/com/nalepa/ticketing/ticketing/infra/http/orderService/OrderServiceClientConfiguration.kt
+++ business/a-ticketing-service/src/main/kotlin/com/nalepa/ticketing/ticketing/infra/http/orderService/OrderServiceClientConfiguration.kt
@@ -1,5 +1,7 @@
 package com.nalepa.ticketing.ticketing.infra.http.orderService
 
+import org.springframework.beans.factory.ObjectProvider
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,10 +10,20 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 class OrderServiceClientConfiguration {
 
+
+    // in this approach, passing tracing headers does not work, dunno XD
+//    @Bean
+//    @LoadBalanced
+//    fun webClientBuilder(): WebClient.Builder {
+//        return WebClient.builder();
+//    }
+
     @Bean
     @LoadBalanced
-    fun webClientBuilder(): WebClient.Builder {
-        return WebClient.builder();
+    fun webClientBuilder(customizers: ObjectProvider<WebClientCustomizer>): WebClient.Builder {
+        val builder = WebClient.builder()
+        customizers.orderedStream().forEach { it.customize(builder) }
+        return builder
     }
 
     @Bean
--- business/b-order-service/src/main/kotlin/com/nalepa/ticketing/order/infra/http/reportingService/ReportingServiceClientConfiguration.kt
+++ business/b-order-service/src/main/kotlin/com/nalepa/ticketing/order/infra/http/reportingService/ReportingServiceClientConfiguration.kt
@@ -1,5 +1,7 @@
 package com.nalepa.ticketing.order.infra.http.reportingService
 
+import org.springframework.beans.factory.ObjectProvider
+import org.springframework.boot.web.client.RestClientCustomizer
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,10 +11,21 @@ import org.springframework.web.client.RestClient
 class ReportingServiceClientConfiguration {
 
 
+    // in this approach, passing tracing headers does not work, dunno XD
+//    @Bean
+//    @LoadBalanced
+//    fun restClientBuilder(): RestClient.Builder {
+//        return RestClient.builder();
+//    }
+
     @Bean
     @LoadBalanced
-    fun restClientBuilder(): RestClient.Builder {
-        return RestClient.builder();
+    fun restClientBuilder(
+        customizers: ObjectProvider<RestClientCustomizer>
+    ): RestClient.Builder {
+        val builder = RestClient.builder()
+        customizers.orderedStream().forEach { it.customize(builder) }
+        return builder
     }
 
     @Bean
